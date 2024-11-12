7852b3eeabee420f2e10521e35c0b970dd7b6c58
Gytis <djgytis231@gmail.com>
2024-11-12 12:30:00 +0200

    Update game again

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1731407146|122037196';
+const CACHE_VERSION = '1731407365|341409177';
 /** @type {string} */
 const CACHE_PREFIX = 'Clarity Quest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
