04b21d4fb335c08c696e45e825ec050763a232b3
Gytis <djgytis231@gmail.com>
2024-12-04 09:45:11 +0200

    Update a lot of things

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1732184909|4806693145';
+const CACHE_VERSION = '1733298254|1456581401';
 /** @type {string} */
 const CACHE_PREFIX = 'Clarity Quest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
