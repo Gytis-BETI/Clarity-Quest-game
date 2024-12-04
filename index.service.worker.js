250378fb66569f267f3ed55a867a20f2f7ebc1c8
Gytis <djgytis231@gmail.com>
2024-12-04 21:55:15 +0200

    Naujas takelis ir naujas žmogeliukas

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1733298254|1456581401';
+const CACHE_VERSION = '1733333342|246955893';
 /** @type {string} */
 const CACHE_PREFIX = 'Clarity Quest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
