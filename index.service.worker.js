8e0df61c5de7888b6a69555feff5d39e57e146f6
Gytis <djgytis231@gmail.com>
2024-12-19 16:05:39 +0200

    Add downlaodable Badges and Level 1 background Plants

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734098983|17045516314';
+const CACHE_VERSION = '1734616895|7447256804';
 /** @type {string} */
 const CACHE_PREFIX = 'Clarity Quest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
