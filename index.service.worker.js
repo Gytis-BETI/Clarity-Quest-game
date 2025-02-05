511d0323d9e97fa6a369ea65f750a7b65393f87b
Gytis <djgytis231@gmail.com>
2025-02-05 19:17:39 +0200

    Fix still fixing the touchscreen and keyboard input

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738775003|4759184292';
+const CACHE_VERSION = '1738775793|341517409';
 /** @type {string} */
 const CACHE_PREFIX = 'Clarity Quest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
